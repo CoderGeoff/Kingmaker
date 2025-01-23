0b0690b22a817f1f89d949758316b92ad2b2efe6
Geoff Bones <Geoff.Bones@siemens.com>
2025-01-23 11:47:06 +0000

    Failing test for travelling two spaces

--- Tests/BoardTests.cs
+++ Tests/BoardTests.cs
@@ -22,6 +22,30 @@ public class BoardTests
         Assert.That(asString, Is.EqualTo(expectedDestinations), asString);
     }
 
+    [TestCase(1, "2(1), 3(0), 5(1), 6(1), 7(0), 9(0), 10(0), 11(0)")]
+    [TestCase(6, "1(1), 2(1), 3(1), 4(0), 5(1), 7(1), 8(0), 9(1), 10(1), 11(1), 12(0)")]
+    public void GivenASimpleSquareBoard_Travel2Spaces_ShouldReturnTheExpectedDestinations(int startingTile, string expectedDestinations)
+    {
+        var tiles = Enumerable.Range(1, 20).Select(id => new Tile(id)).ToArray();
+
+        var board = new BoardBuilder().WithTiles(tiles).WithLayout([(1, [2, 5, 6]),
+                                                                    (2, [3, 5, 6, 7]),
+                                                                    (3, [4, 6, 7, 8]),
+                                                                    (4, [7, 8]),
+                                                                    (5, [6, 9, 10]),
+                                                                    (6, [7, 9, 10, 11]),
+                                                                    (7, [8, 10, 11, 12]),
+                                                                    (8, [11, 12]),
+                                                                    (9, [10]),
+                                                                    (10, [11]),
+                                                                    (11, [12])])
+                                      .Build();
+        var start = board.GetTile(startingTile);
+        var destinations = start.Travel(2);
+        var asString = AsString(destinations);
+        Assert.That(asString, Is.EqualTo(expectedDestinations), asString);
+    }
+
     private static string AsString(IEnumerable<(Tile destination, int distanceLeft)> destinations)
     {
         return destinations.Select(d => $"{d.destination.Id}({d.distanceLeft})").ToCommaSeparatedList();
