9821efc40f4d2f8ae08d7db92d51b3800b0ac8df
Geoff Bones <Geoff.Bones@siemens.com>
2025-01-23 11:43:54 +0000

    Fix tests

--- Tests/BoardTests.cs
+++ Tests/BoardTests.cs
@@ -5,13 +5,14 @@ namespace Tests;
 
 public class BoardTests
 {
-    [TestCase(1, "2(0), 3(0), 5(0)")]
+    [TestCase(1, "2(0), 5(0), 6(0)")]
     [TestCase(2, "1(0), 3(0), 5(0), 6(0), 7(0)")]
+    [TestCase(6, "1(0), 2(0), 3(0), 5(0), 7(0), 9(0), 10(0), 11(0)")]
     public void GivenASimpleSquareBoard_Travel1Space_ShouldReturnTheExpectedDestinations(int startingTile, string expectedDestinations)
     {
         var tiles = Enumerable.Range(1, 20).Select(id => new Tile(id)).ToArray();
 
-        var board = new BoardBuilder().WithTiles(tiles).WithLayout([ (1, [ 2, 3, 5 ]), (2, [ 3, 5, 6, 7 ]), (3, [ 4, 6, 7, 8 ]), (4, [ 7, 8 ]),
+        var board = new BoardBuilder().WithTiles(tiles).WithLayout([ (1, [ 2, 5, 6 ]), (2, [ 3, 5, 6, 7 ]), (3, [ 4, 6, 7, 8 ]), (4, [ 7, 8 ]),
                                                                      (5, [ 6, 9, 10 ]), (6, [ 7, 9, 10, 11 ]), (7, [ 8, 10, 11, 12 ]), (8, [ 11, 12 ]),
                                                                      (9, [ 10 ]), (10, [ 11 ]), (11, [ 12 ]) ])
                                       .Build();
